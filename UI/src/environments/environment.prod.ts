1f1ca0e4abe8b0641cab03e016c4efc3bf138d8e
Praveen Benny <praveenbenny007@gmail.com>
2025-11-25 01:15:19 +0530

    added environment.ts anonkey

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    supabaseUrl: 'YOUR_SUPABASE_URL',
-    supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
+    supabaseUrl: 'https://lpvljmastznpzvzjvuyf.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwdmxqbWFzdHpucHp2emp2dXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5OTk0NTIsImV4cCI6MjA3OTU3NTQ1Mn0.Sumq7dW-wEsldi38FtqHbXNWULxjGhUuFs08sQ_1TVw'
+
 };
--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: '',
-    supabaseKey: ''
+    supabaseUrl: 'https://lpvljmastznpzvzjvuyf.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwdmxqbWFzdHpucHp2emp2dXlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5OTk0NTIsImV4cCI6MjA3OTU3NTQ1Mn0.Sumq7dW-wEsldi38FtqHbXNWULxjGhUuFs08sQ_1TVw'
 };
